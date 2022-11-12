587d6d6bdee99b40dfae71acc7ed0b3cbde4117b
Matthew Larrosa <matthewterra16@gmail.com>
2022-11-12 00:18:39 -0500

    implemented grid

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-function makeGrid(){
-    let gridCanvas = document.getElementById('pixelCanvas');
+function makeMap() {
+  let map = document.getElementById("pixelMap");
 
-    for(let i = 0; i < 10; i++){
-        let row = document.getElementById('tr');
-        row.id = "row" + 1;
+  for (let i = 0; i < 10; i++) {
+    let rows = document.createElement("tr");
+    rows.id = "row" + i;
+
+    map.appendChild(rows);
+
+    let rowWidth = document.getElementById("row" + i);
+
+    for (let j = 0; j < 10; j++) {
+      let width = document.createElement("td");
+      rowWidth.appendChild(width);
     }
-    gridCanvas.appendChild(row);
+  }
 }
